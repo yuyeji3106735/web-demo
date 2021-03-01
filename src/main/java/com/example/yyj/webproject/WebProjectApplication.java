7cf142f14190403edbb8b40a5e4aa2e808eaef1b
yuyeji3106735 <417998246@qq.com>
2021-03-01 16:48:09 +0800

    [*]添加tomcat配置

--- src/main/java/com/example/yyj/webproject/WebProjectApplication.java
+++ src/main/java/com/example/yyj/webproject/WebProjectApplication.java
@@ -2,12 +2,18 @@ package com.example.yyj.webproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebProjectApplication {
+public class WebProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebProjectApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebProjectApplication.class);
+    }
 }
